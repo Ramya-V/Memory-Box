0100aaf2ceb7643ebd147864a9d98ea01e7c5985
Ramya-V <rajaramya559@gmail.com>
2020-07-30 13:11:11 +0530

    [IMP] Home : Navigation of FAB to Add up Modal is done

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPage implements OnInit {
 
-  constructor() { }
+  constructor(
+  ) { }
 
   ngOnInit() {
   }
-
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AddPageModule } from '../add/add.module';
+import { AddPage } from '../add/add.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+      public modal : ModalController
+  ) {}
 
+  //Add up Modal page 
+  addup(){
+    this.modal.create({component: AddPage}).then((ModalElement)=>{
+      ModalElement.present();
+    })
+  }
 }
